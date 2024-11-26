7f3bc57cc4bc6b59a90c4e3936154ca49731bc61
tea06194 <tea06194@gmail.com>
2024-11-27 00:28:59 +0300

    hello with errors tag v0.0.1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tea06194/hello
+
+go 1.22.4
+
+require github.com/tea06194/greetings v0.0.4
--- hello.go
+++ hello.go
@@ -1,1 +1,17 @@
-package hello
+package main
+
+import (
+	"fmt"
+
+	"github.com/tea06194/greetings"
+)
+
+func main() {
+	message, error := greetings.Hello("qwe")
+
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	fmt.Println(message)
+}
